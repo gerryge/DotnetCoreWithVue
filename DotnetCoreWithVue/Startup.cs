ca5ce47ec83279a0fde5589e7767fc36fdb274ba
Gerry Ge <99432281@qq.com>
2021-10-31 17:06:28 +0800

    Add pack

--- DotnetCoreWithVue/Startup.cs
+++ DotnetCoreWithVue/Startup.cs
@@ -25,7 +25,11 @@ namespace DotnetCoreWithVue
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
-      services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+      services.AddMvc(mvcOptions =>
+      {
+        mvcOptions.EnableEndpointRouting = false;
+      }
+        ).SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
